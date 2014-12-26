30487230a29dcd15e264a22c724b88ed578dd755
Hermione <hermione0521@gmail.com>
2014-12-26 15:43:23 +0800

    fill post method in register route

--- routes/register.js
+++ routes/register.js
@@ -1,17 +1,33 @@
 var express = require('express'),
 	router = express.Router(),
-	go = require('../globalObjects');
+	go = require('../globalObjects'),
+	IndividualService = require('../service/IndividualService'),
+	OrganizationService = require('../service/OrganizationService');
 
 /*
  * register an individual user
  */
 router.post('/individual', function (req, res) {
+	IndividualService.register(req.body.username,
+								req.body.password,
+								req.body.email,
+								req.body.mobile, function(result){
+		res.send(result);
+	});
 });
 
 /*
  * register an organization user
  */
 router.post('/organization', function (req, res) {
+	OrganizationService.register(req.body.username,
+								req.body.password,
+								req.body.email,
+								req.body.phone,
+								req.body.orgName,
+								req.body.orgNum, function(result) {
+		res.send(result);
+	});
 });
 
 module.exports = router;
