56fa550c61725dc00bbea32277d89efb728af515
Hermione <hermione0521@gmail.com>
2014-12-28 14:46:28 +0800

    modify index route && add front files

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,19 @@ var express = require('express'),
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	projectService.latestProject(6, function(latest) {
-		for (var i = 0; i < latest.length; ++i) {
-			latest[i].ratio = latest[i].moneyRaised / latest[i].moneyNeeded;
+	projectService.latestProject(6, function(ans) {
+		if (ans.success) {
+			var latest = ans.message;
+			latest = []; // for debug
+			for (var i = 0; i < latest.length; ++i) {
+				latest[i].ratio = latest[i].moneyRaised / latest[i].moneyNeeded;
+			}
+			console.dir(latest);
+			res.render('index', {latest: latest});
 		}
-		res.render('index', {latest: latest});
+		else {
+			console.log(ans.message);
+		}
 	});
 });
 
